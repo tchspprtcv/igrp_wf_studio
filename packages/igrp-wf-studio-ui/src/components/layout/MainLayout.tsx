5f6592c5a902be0889315086db63d9ccbe8e161a
NOSI / DDS - Francisco Lopes Horta <francisco.horta@nosi.cv>
2025-05-07 14:33:07 -0100

    refactor: adjust MainLayout styling and enable composite in tsconfig
    Update the MainLayout component to simplify the padding structure and enable the composite option in the tsconfig.json file to support project references

--- packages/igrp-wf-studio-ui/src/components/layout/MainLayout.tsx
+++ packages/igrp-wf-studio-ui/src/components/layout/MainLayout.tsx
@@ -112,8 +112,8 @@ const MainLayout: React.FC = () => {
         {/* Header needs to be outside the potential PanelGroup of main content if we want it full width */}
         <Header onMobileMenuClick={toggleMobileMenu} />
         <main className="flex-1 overflow-y-auto">
-          <div className="py-6">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div>
+            <div className=" mx-auto px-4 py-6">
               <Outlet />
             </div>
           </div>
